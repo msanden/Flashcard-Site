f3c2f0b5dd82ab14759b802250882fc2c5c3efa9
Manuel Sande <mnlsande22@gmail.com>
2019-03-17 08:14:22 -0400

    Add New File

---
+++ js/scripts.js
@@ -0,0 +1,98 @@
+
+$(document).ready(function() {
+
+ $('.clickable').click(function() {
+    $('#javascript-hidden').toggle();
+    $('#javascript-showing').toggle();
+  });
+
+  $('.clickable1').click(function() {
+    $('#operator-hidden').toggle();
+    $('#operator-showing').toggle();
+  });
+
+  $('.clickable2').click(function() {
+    $('#variables-hidden').toggle();
+    $('#variables-showing').toggle();
+  });
+
+  $('.clickable3').click(function() {
+    $('#namingConventions-hidden').toggle();
+    $('#namingConventions-showing').toggle();
+  });
+
+  $('.clickable4').click(function() {
+    $('#functions-hidden').toggle();
+    $('#functions-showing').toggle();
+  });
+
+  $('.clickable5').click(function() {
+    $('#strings-hidden').toggle();
+    $('#strings-showing').toggle();
+  });
+
+  $('.clickable6').click(function() {
+    $('#methods-hidden').toggle();
+    $('#methods-showing').toggle();
+  });
+
+  $('.clickable7').click(function() {
+    $('#arguments-hidden').toggle();
+    $('#arguments-showing').toggle();
+  });
+
+  $('.clickable8').click(function() {
+    $('#parameters-hidden').toggle();
+    $('#parameters-showing').toggle();
+  });
+
+  $('.clickable9').click(function() {
+    $('#return-hidden').toggle();
+    $('#return-showing').toggle();
+  });
+
+  $('.clickable10').click(function() {
+    $('#chainingMethods-hidden').toggle();
+    $('#chainingMethods-showing').toggle();
+  });
+
+  $('.clickable11').click(function() {
+    $('#booleans-hidden').toggle();
+    $('#booleans-showing').toggle();
+  });
+
+  $('.clickable12').click(function() {
+    $('#undefined-hidden').toggle();
+    $('#undefined-showing').toggle();
+  });
+
+  $('.clickable13').click(function() {
+    $('#NaN-hidden').toggle();
+    $('#NaN-showing').toggle();
+  });
+
+  $('.clickable14').click(function() {
+    $('#escape-hidden').toggle();
+    $('#escape-showing').toggle();
+  });
+
+  $('.clickable15').click(function() {
+    $('#alert-hidden').toggle();
+    $('#alert-showing').toggle();
+  });
+
+  $('.clickable16').click(function() {
+    $('#comments-hidden').toggle();
+    $('#comments-showing').toggle();
+  });
+
+  $('.clickable17').click(function() {
+    $('#jQuery-hidden').toggle();
+    $('#jQuery-showing').toggle();
+  });
+
+  $('.clickable18').click(function() {
+    $('#attributes-hidden').toggle();
+    $('#attributes-showing').toggle();
+  });
+});
